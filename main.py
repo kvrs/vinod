bc621e809dd8825b993753d210caf02b8280fac7
kvrs <43546720+kvrs@users.noreply.github.com>
2024-09-02 23:09:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Replace "*" with your frontend's domain if needed
+    allow_origins=["*"],  # Replace "*" with your frontend's domain 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
