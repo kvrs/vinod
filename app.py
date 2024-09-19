eea58a9364335ada3de43e0c8392b145bfcd12c0
kvrs <43546720+kvrs@users.noreply.github.com>
2024-09-19 10:26:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from google.cloud.devtools import cloudbuild_v1
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-def trigger_cloud_build(project_id, branch_name, trigger_id, substitutions=None):
+def trigger_cloud_build(project_id, branch_name, trigger_id, user_id=None,job_id=None, file_path=None, substitutions=None):
     client = cloudbuild_v1.CloudBuildClient()
     repo_source = cloudbuild_v1.RepoSource(branch_name=branch_name)
     build_trigger_request = cloudbuild_v1.RunBuildTriggerRequest(
@@ -13,6 +13,12 @@ def trigger_cloud_build(project_id, branch_name, trigger_id, substitutions=None)
         trigger_id=trigger_id,
         source=repo_source
     )
+    
+    # Print the user_id for logging purposes (you can use it as needed)
+    print(f"Build triggered by user: {user_id}")
+    print(f"Build triggered by job id: {job_id}")
+    print(f"Build triggered by file path : {file_path}")
+
     operation = client.run_build_trigger(request=build_trigger_request)
     response = operation.result()
 
@@ -25,13 +31,16 @@ def trigger_build():
     project_id = data.get('project_id')
     branch_name = data.get('branch_name')
     trigger_id = data.get('trigger_id')
-
+    user_id = data.get('user_id')  # Capture user_id from the request
+    job_id = data.get('job_id')  # Capture user_id from the request
+    file_path = data.get('file_path')  # Capture user_id from the request
+    
     if not project_id or not branch_name or not trigger_id:
         return jsonify({"error": "Missing required parameters"}), 400
 
     try:
-        response = trigger_cloud_build(project_id, branch_name, trigger_id)
-        return jsonify({"message": f"Build triggered: {response.id}"})
+        response = trigger_cloud_build(project_id, branch_name, trigger_id, user_id=user_id,job_id=job_id,file_path=file_path)
+        return jsonify({"message": f"Build triggered: {response.id}", "user_id": user_id, "job_id": job_id, "file_path": file_path})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
