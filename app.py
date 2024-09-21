603f57f80308ddb95b559921c295374a60bd49c4
kvrs <43546720+kvrs@users.noreply.github.com>
2024-09-21 05:46:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,5 +45,6 @@ def trigger_build():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=port)
